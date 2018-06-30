c981a78c469a8a7853a1347571d6f2f3833342c8
root <root@ntw-k8s-master01.maas>
2018-07-01 03:30:51 +0900

    hide cluster context

--- kube-prompt-bash.sh
+++ kube-prompt-bash.sh
@@ -5,5 +5,6 @@ kube_prompt() {
   kube_cluster=$(echo "$kube_current_context" | grep "    cluster:" | awk '{print $2}')
   kube_namespace=$(echo "$kube_current_context" | grep "    namespace:" | awk '{print $2}')
   kube_user=$(echo "$kube_current_context" | grep "    user:" | awk '{print $2}')
-  echo $kube_cluster $kube_namespace $kube_user
+  #echo $kube_cluster $kube_namespace $kube_user
+  echo $kube_namespace $kube_user
 }
